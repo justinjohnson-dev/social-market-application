d2dea6aa6edd025a2fba868267003ecb2d4f72e5
Myra Ausherman <myraausherman@Myras-MacBook.local>
2020-10-16 10:55:55 -0400

    header folder add

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from '../navigation/Navbar';
-import Header from '../../header/Header';
+import Header from '../header/Header';
 import { render } from '@testing-library/react';
 import { Link } from "react-router-dom";
 import './home.css';
