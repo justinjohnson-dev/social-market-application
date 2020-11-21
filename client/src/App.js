bc4fad9739041bc3f0f50014a5315f0ae844d835
Justin Johnson <jjustin634@gmail.com>
2020-11-21 13:12:14 -0600

    Update App.js
    removing baseline route

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router basename="/React">
+        <Router>
           <div className="App">
             <Navbar />
             <Header />
