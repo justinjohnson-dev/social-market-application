138e1d11d7085eea415864edc76d92ad1305f91a
Justin Johnson <jjustin634@gmail.com>
2020-11-23 23:02:24 -0600

    Update config.js
    removing key info - trying to edit file directly on server, having issues.

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: 'mongodb+srv://basic_user:y4yEq8NXJR8BZ1Wk@cluster0.w2wff.mongodb.net/homegrown?retryWrites=true&w=majority',
+    db: '',
     port: 5000,
     secretOrKey: "secret"
 };
