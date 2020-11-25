f5feee1be87dec0533b6d0c4f8f8fab9a963ccd8
Justin Johnson <jjustin634@gmail.com>
2020-11-24 21:48:34 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://basic_user:y4yEq8NXJR8BZ1Wk@cluster0.w2wff.mongodb.net/homegrown?retryWrites=true&w=majority',
     port: 5000,
     secretOrKey: "secret"
 };
