e61a0cb266f7067aeb6ac32b725721905c0150c5
LoganBecker <56939121+LoganBecker@users.noreply.github.com>
2020-12-10 23:00:35 -0600

    Adding Footer to this remote branch
    Myra wrote this footer.
    
    Co-Authored-By: maushermful <50720196+maushermful@users.noreply.github.com>

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+
+
+function Footer () {
+    return (
+        <div className = "Footer">
+            <center>
+            Home Grown Social App est. 2020
+            </center>
+            </div>
+    )
+}
+
+
+
+export default Footer;
